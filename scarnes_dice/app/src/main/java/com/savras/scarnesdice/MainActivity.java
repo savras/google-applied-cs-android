585de95003097b203f4e4bf2208258074b3190a4
savras <d20.savras@gmail.com>
2018-02-19 00:58:41 +0800

    gcs: WIP scarnes dice module logic 1

--- scarnes_dice/app/src/main/java/com/savras/scarnesdice/MainActivity.java
+++ scarnes_dice/app/src/main/java/com/savras/scarnesdice/MainActivity.java
@@ -1,13 +1,54 @@
 package com.savras.scarnesdice;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int userTotalScore;
+    private int userTurnScore;
+    private int pcTotalScore;
+    private int pcTurnScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void roll(View view) {
+        Random ran = new Random();
+        int roll = ran.nextInt(5) + 1;
+
+        ImageView imageView = findViewById(R.id.imageView);
+        switch(roll) {
+            case 1: {
+                imageView.setImageResource(R.drawable.dice1);
+            }
+            case 2: {
+                imageView.setImageResource(R.drawable.dice2);
+            }
+            case 3: {
+                imageView.setImageResource(R.drawable.dice3);
+            }
+            case 4: {
+                imageView.setImageResource(R.drawable.dice4);
+            }
+            case 5: {
+                imageView.setImageResource(R.drawable.dice5);
+            }
+            case 6: {
+                imageView.setImageResource(R.drawable.dice6);
+            }
+        }
+
+        TextView textView = findViewById(R.id.scorePointTextView);
+        textView.setText(roll);
     }
 }
